90bb02ba0fc17b3913693f6260c44306a424f08d
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-04-25 12:53:36 -0400

    remove MaineCat option

--- web/wp-content/plugins/colby-libraries/templates/search-tabs/books-and-more.php
+++ web/wp-content/plugins/colby-libraries/templates/search-tabs/books-and-more.php
@@ -13,28 +13,6 @@
 	</a>
 </form>
 
-<!-- MAINECAT -->
-<form id=tab2_form3 class=option3 action=https://mainecat.maine.edu/search/ name=search>
-	<input type=hidden id=iiiFormHandle_1>
-	<div class=formEntryArea>
-		<input type=text
-			   class=summon-search-field
-			   placeholder="<?php echo $colby_libraries->placeholder_text; ?>"
-			   maxlength=75
-			   name=searcharg
-			   size=30>
-		<span class="nowrap formButtonArea">
-			<input type=hidden id=iiiFormHandle_1>
-			<input type=submit value="Search >">
-		</span>
-		<select name=searchtype id=searchtype>
-			<option value=X selected>Keyword
-			<option value=t>Title
-			<option value=a>Author (Last, First)
-		</select>
-	</div>
-</form>
-
 <!-- NEXPRESS -->
 
 <form name=wcfw id=tab2_form4 class=option4 accept-charset=UTF-8 action=//www.worldcat.org/search>
@@ -51,10 +29,6 @@
 		<input data-inputid=1 name=tab2_option1 checked id=tab2_option1 type=radio class=option1>
 		CBBCat
 	</label>
-	<label for=tab2_option3>
-		<input id=tab2_option3 data-inputid=3 name=tab2_option3 type=radio class=option3>
-		MaineCat
-	</label>
 	<label for=tab2_option4>
 		<input id=tab2_option4 data-inputid=4 name=tab2_option4 type=radio class=option4>
 			WorldCat
@@ -67,18 +41,13 @@
 	<p>Books and other materials from Colby, Bates and Bowdoin.</p>
 	
 	<p><a href="https://www.cbbnet.org/new-books-and-media/">
-    Explore NEW Items >
+	Explore NEW Items >
 	  </a></p>
 	<p>
 		<a href=https://colby.libwizard.com/f/New-Suggest-a-Purchase-Form>
-	 		Suggest a purchase >
+			 Suggest a purchase >
 		</a></p>
 		
-</div>
-<div class="bottom-text option3">
-	<strong>What's this?</strong>
-	<p>Academic and public libraries in Maine</p>
-	<a href=https://www.maineinfonet.org/mainecat/about/>Help with MaineCat</a>
 </div>
 <div class="bottom-text option4">
 	<strong>What's this?</strong>
