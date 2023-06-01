270592552fd383a57579211c3859b43667b0c053
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-06-01 10:13:20 -0400

    fix span tags and add functionality for research guide search in colby-libraries plugin

--- web/wp-content/plugins/colby-libraries/templates/search-tabs/research-guides.php
+++ web/wp-content/plugins/colby-libraries/templates/search-tabs/research-guides.php
@@ -1,21 +1,18 @@
-<form class=option1
-	id=search-cbbcat
-	name=search-cbbcat
-	action=https://colby.summon.serialssolutions.com/search
+<form
+	id="search-research-guide"
+	name="rgForm"
 	class=summon-search-widget
 	accept-charset=utf-8
-	id=sb1a9a060f7150131756b2ae8d1b2df50>
-  <input name=utf8 type=hidden value=✓>
+	method="GET"
+	action="https://libguides.colby.edu/srch.php">
 	<div id="search-box">
 		<input type=text
 			placeholder="Find a research guide by subject or course"
 			class="summon-search-field search-bar-full"
-			name=s.q
+			name="q"
 			autocomplete=off>
 		<input type=image src="/wp-content/plugins/colby-libraries/assets/img/search.svg" alt="search">
 	</div>
-  <input type=hidden name=s.fvf[] value="ContentType,Newspaper Article,t">
-  <input type=hidden name=keep_r value=true>
 </form>
 
 <section class="content-area">
@@ -34,8 +31,8 @@
 
 <div class="search-footer">
 	<ul class="search-footer-links">
-		<li><a href="https://libanswers.colby.edu/"><img src='/wp-content/plugins/colby-libraries/assets/img/get_help.svg' alt="Get Help"><span>get help</span</a></li>
-		<li><a href="https://libraries.colby.edu/ill/"><img src='/wp-content/plugins/colby-libraries/assets/img/interlibrary_loan.svg' alt="Interlibrary Loan"><span>interlibrary loan</span</a></li>
-		<li><a href="https://libraries.colby.edu/remote-library-access"><img src='/wp-content/plugins/colby-libraries/assets/img/remote_access.svg' alt="Remote Access"><span>remote access</span</a></li>
+		<li><a href="https://libanswers.colby.edu/"><img src='/wp-content/plugins/colby-libraries/assets/img/get_help.svg' alt="Get Help"><span>get help</span></a></li>
+		<li><a href="https://libraries.colby.edu/ill/"><img src='/wp-content/plugins/colby-libraries/assets/img/interlibrary_loan.svg' alt="Interlibrary Loan"><span>interlibrary loan</span></a></li>
+		<li><a href="https://libraries.colby.edu/remote-library-access"><img src='/wp-content/plugins/colby-libraries/assets/img/remote_access.svg' alt="Remote Access"><span>remote access</span></a></li>
 	</ul>
 </div>
