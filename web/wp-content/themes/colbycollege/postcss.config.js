84d2500b63bb932991c73744911c1e0aaf7b857f
Brandon K. Waltz <bwaltz6@gmail.com>
2023-08-21 11:44:46 -0400

    mock launch for redesign branch

--- web/wp-content/themes/colbycollege/postcss.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const autoprefixer = require('autoprefixer');
-const customPropFallbacks = require('postcss-custom-prop-fallbacks');
-
-module.exports = {
-  plugins: [autoprefixer, customPropFallbacks],
-};
