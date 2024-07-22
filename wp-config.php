050b542bb5d3df4bc064a10d352eda1542f64a96
Brandon K. Waltz <bwaltz6@gmail.com>
2024-07-22 15:44:15 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -124,10 +124,10 @@ if ( false !== $strRelationships = getenv( 'PLATFORM_RELATIONSHIPS' ) ) {
 	// Debug mode should be disabled on Platform.sh. Set this constant to true
 	// in a wp-config-local.php file to skip this setting on local development.
 	if ( ! defined( 'WP_DEBUG' ) ) {
-		define( 'WP_DEBUG', true );
+		define( 'WP_DEBUG', false );
 		ini_set( 'display_errors', 'On' );
 		ini_set( 'error_reporting', E_ALL );
-		define( 'WP_DEBUG_DISPLAY', true );
+		define( 'WP_DEBUG_DISPLAY', false );
 	}
 
 	// Set all of the necessary keys to unique values, based on the Platform.sh
