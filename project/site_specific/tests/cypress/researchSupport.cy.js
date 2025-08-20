afe1ea8ee9a947f7475c91c0181b49e2e2cee6a7
Brandon K. Waltz <bwaltz6@gmail.com>
2025-08-20 14:56:35 -0400

    Update researchSupport.cy.js

--- project/site_specific/tests/cypress/researchSupport.cy.js
+++ project/site_specific/tests/cypress/researchSupport.cy.js
@@ -1,7 +1,7 @@
 describe('Research Support Accordions', () => {
     it('Should open accordion', () => {
         cy.visit('/research-support/');
-        cy.get('.colby-accordion-block article:first-of-type').click({
+        cy.get('.colby-accordion-block article:first-of-type .accordion__button').click({
             force: true,
         });
         cy.get('.colby-accordion-block article:first-of-type > .accordion__window')
